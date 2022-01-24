889b53643300ad75bcac381131ecee8f53b782cb
greenddoovie <mania01234@naver.com>
2022-01-24 15:51:13 +0900

    modify: 초기 화면 구성
    1. 클래스명, 패키지명 변경
    2. Bottom Navigation View 삭제
    3. NoActionBar 설정

--- app/src/main/java/com/example/remindapp/ui/edit/EditFragment.kt
+++ app/src/main/java/com/example/remindapp/ui/edit/EditFragment.kt
@@ -1,4 +1,4 @@
-package com.example.remindapp.ui.dashboard
+package com.example.remindapp.ui.edit
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.remindapp.R
-import com.example.remindapp.databinding.FragmentDashboardBinding
+import com.example.remindapp.databinding.FragmentEditBinding
+import com.example.remindapp.ui.Edit.EditViewModel
 
-class DashboardFragment : Fragment() {
+class EditFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var dashboardViewModel: EditViewModel
+    private var _binding: FragmentEditBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,9 +26,9 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(EditViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentEditBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
