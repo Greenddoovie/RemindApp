9c3c5da273f9580348a046fe7bcaabd5b74393e5
greenddoovie <mania01234@naver.com>
2022-01-24 16:27:49 +0900

    feat: MainFragment 리마인더 추가 버튼
    1. XML 선언
    2. Navigation Action 추가
    3. main fragment 내에 button listener 등록
    4. drawable plus 추가

--- app/src/main/java/com/example/remindapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/remindapp/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.remindapp.R
 import com.example.remindapp.databinding.FragmentHomeBinding
 
@@ -16,15 +17,13 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -38,8 +37,20 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setButtonClickListener()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setButtonClickListener() {
+        binding.btAddRemind.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_navigation_edit)
+        )
+    }
+
 }
