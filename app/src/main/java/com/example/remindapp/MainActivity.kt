da45b1d24772fd2611956c953be97bdde53344c4
greenddoovie <mania01234@naver.com>
2022-01-27 22:32:31 +0900

    feat: 화면 꺼졌을 때, 알람 울리면 화면 켜지도록 구현

--- app/src/main/java/com/example/remindapp/MainActivity.kt
+++ app/src/main/java/com/example/remindapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.remindapp
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -46,9 +48,19 @@ class MainActivity : AppCompatActivity() {
             val id = idx as Int
             val bundle = Bundle()
             bundle.putInt("remindIdx", id)
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
             val navController = navHostFragment.navController
             navController.navigate(R.id.action_navigation_home_to_navigation_notifications, bundle)
+        }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        if (Build.VERSION.SDK_INT >= 27) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        } else{
+            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
         }
     }
 }
