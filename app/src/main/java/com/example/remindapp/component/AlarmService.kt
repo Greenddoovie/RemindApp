8eabe8f116e50a10d6da1f4b540920ac3f88afed
greenddoovie <mania01234@naver.com>
2022-01-27 16:34:55 +0900

    modify: Service 에러 해결 및
    1. startForegroundService로 실행한 service는 내부에서 5초 이내에 startForeground 함수를 이용해서 notification을 띄워야함

--- app/src/main/java/com/example/remindapp/component/AlarmService.kt
+++ app/src/main/java/com/example/remindapp/component/AlarmService.kt
@@ -2,7 +2,6 @@ package com.example.remindapp.component
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -14,7 +13,6 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.net.toUri
 import com.example.remindapp.MainActivity
 import com.example.remindapp.R
-import com.example.remindapp.SecondActivity
 import com.example.remindapp.model.repository.RemindLocalDatasource
 import com.example.remindapp.model.repository.RemindRepository
 import com.example.remindapp.model.room.RemindDatabase
@@ -27,11 +25,16 @@ class AlarmService : Service() {
 
     private lateinit var repo: RemindRepository
     private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(applicationContext)
+            notifyNotification(applicationContext)
+        }
+    }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if (intent == null) return START_NOT_STICKY
-
-        createNotificationChannel(applicationContext)
-        notifyNotification(applicationContext)
 
         val idx = intent.extras?.get("remindIdx") ?: -1
         val onOff = intent.extras?.get("alarm") ?: "off"
@@ -42,12 +45,15 @@ class AlarmService : Service() {
                 val remind = repo.getRemind(idx as Int)
                 mediaPlayer = MediaPlayer.create(applicationContext, remind.uri.toUri())
                 mediaPlayer.start()
+                mediaPlayer.isLooping = true
+
                 withContext(Dispatchers.Main) {
-                    val activityIntent = Intent(applicationContext, SecondActivity::class.java)
+                    val activityIntent = Intent(applicationContext, MainActivity::class.java)
                     activityIntent.putExtra("remindIdx", remind.id)
                     activityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                     applicationContext.startActivity(activityIntent)
                 }
+
             }
 
             return START_NOT_STICKY
@@ -57,6 +63,7 @@ class AlarmService : Service() {
 
     override fun onDestroy() {
         mediaPlayer.stop()
+        mediaPlayer.release()
         super.onDestroy()
     }
 
@@ -78,14 +85,12 @@ class AlarmService : Service() {
     }
 
     private fun notifyNotification(context: Context) {
-        with(NotificationManagerCompat.from(context)) {
-            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setContentText("알람")
-                .setContentText("리마인드 시간")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setSmallIcon(R.drawable.alarm_24)
-            notify(NOTIFICATION_ID, build.build())
-        }
+        val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setContentText("알람")
+            .setContentText("리마인드 시간")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.drawable.alarm_24)
+        startForeground(NOTIFICATION_ID, build.build())
     }
 
     companion object {
