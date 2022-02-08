840c6c0b6551bded0b5389f2b3679bd4740de076
greenddoovie <mania01234@naver.com>
2022-02-08 23:15:02 +0900

    refactor: 블록이 본문인 함수에서 식이 본문인 함수로 변경

--- app/src/main/java/com/example/remindapp/model/repository/RemindLocalDatasource.kt
+++ app/src/main/java/com/example/remindapp/model/repository/RemindLocalDatasource.kt
@@ -4,17 +4,11 @@ import com.example.remindapp.model.room.Remind
 import com.example.remindapp.model.room.RemindDatabase
 
 class RemindLocalDatasource(private val remindDatabase: RemindDatabase) : IRemindRepo {
-    override fun getAll(): List<Remind> {
-        return remindDatabase.remindDao().getAll()
-    }
+    override fun getAll(): List<Remind> = remindDatabase.remindDao().getAll()
 
-    override fun insert(remind: Remind) {
-        remindDatabase.remindDao().insert(remind)
-    }
+    override fun insert(remind: Remind) = remindDatabase.remindDao().insert(remind)
 
-    override fun update(remind: Remind) {
-        remindDatabase.remindDao().update(remind)
-    }
+    override fun update(remind: Remind) = remindDatabase.remindDao().update(remind)
 
     override fun update(
         title: String,
@@ -27,12 +21,9 @@ class RemindLocalDatasource(private val remindDatabase: RemindDatabase) : IRemin
         remindDatabase.remindDao().update(title, hour, minute, uri, active, id)
     }
 
-    override fun getRemind(idx: Int): Remind {
-        return remindDatabase.remindDao().getRemind(idx)
-    }
+    override fun getRemind(idx: Int): Remind = remindDatabase.remindDao().getRemind(idx)
 
-    override fun getRemind(title: String, hour: Int, minute: Int, uri: String): Remind {
-        return remindDatabase.remindDao().getRemind(title, hour, minute, uri)
-    }
+    override fun getRemind(title: String, hour: Int, minute: Int, uri: String): Remind =
+        remindDatabase.remindDao().getRemind(title, hour, minute, uri)
 
 }
